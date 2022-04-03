0a5e6a44151fb25814269ec06a16ca71b8b22373
ralvarez20 <rodrigoalvarez449@gmail.com>
2022-04-02 22:56:23 -0600

    Cambios minimos

--- routes/routers.py
+++ routes/routers.py
@@ -29,6 +29,34 @@ def move_into_routers(child, routers_list, usr, pwd, method, log = None):
             child.expect("Password:")
             child.sendline(pwd)
 
+@netapi_decorator("routers")
+def get_router_protocols(child, host, log = None):
+    child.sendline("terminal length 0")
+    child.expect(host)
+    log.info("Obteniendo protocolos del router")
+    child.sendline("show ip protocol")
+    child.expect(host)
+    protocol_info = child.before.decode()
+    # Regresar la lista de los protocolos validos
+    if protocol_info != "":
+        log.info("Se ha encontrado valores de protocolos")
+        protocol_info = protocol_info.split("\n")
+        routing_protocols_data = [x for x in protocol_info if x.startswith("Routing Protocol")]
+        protocols_in_router = []
+        for prot in routing_protocols_data:
+            temp_p = prot.split(" ")[3]
+            temp_p = temp_p.replace("\r", "")
+            temp_p = temp_p.replace("\"", "")
+            protocols_in_router.append(temp_p)
+        log.info(f"Protocolos encontrado: {', '.join(protocols_in_router)}")
+        return protocols_in_router
+
+@netapi_decorator("routers")
+def delete_protocols_from_router(child, host, protocols_list, log = None):
+    for pt_rt in protocols_list:
+        log.info(f"Eliminando protocolo: {pt_rt}")
+        child.sendline(f"no router {pt_rt}")
+        child.expect(host)
 
 @netapi_decorator("network", "devices")
 def list_devices_in_db(log = None, db = None):
@@ -101,47 +129,43 @@ def update_user_from_router(log=None):
     usr_to_update = request_body["username"]
     new_pwd = request_body["pwd"]
     privilege = request_body["privilege"]
+    con_method = request_body["method"]
     last_host = hosts[len(hosts)-1]
     log.info(f"Modificando usuario {usr_to_update} en el router con IP: {last_host['ip']}")
-    # Verificar si se va a hacer por SSH y en el caso, obtener las credenciales
-    if ("ssh_usr", "ssh_pwd") in request_body:
-        #Hacerlo por SSH
-        pass
-    else:
-        #Hacerlo via telnet con la cuenta por defecto de todos los routers 
-        telnet_usr, telnet_pwd = get_telnet_config()
-        try:
-            log.info(f"Enviando comandos al router: {hosts[0]['ip']}")
+    gns3_usr, gns3_pwd = get_gns3_config()
+    try:
+        if con_method == "ssh":
+            # Primer inicio de sesion via ssh al primer router
+            child = pxssh.pxssh()
+            child.login(hosts[0]["ip"], gns3_usr, gns3_pwd, auto_prompt_reset=False)
+        else:
+            # Inicio de sesion via telnet al primer router
             child = pexpect.spawn(f"telnet {hosts[0]['ip']}")
             child.expect("Username:")
-            child.sendline(telnet_usr)
+            child.sendline(gns3_usr)
             child.expect("Password:")
-            child.sendline(telnet_pwd)
+            child.sendline(gns3_pwd)
             child.expect(hosts[0]['host'])
-            # Siguientes hosts
-            for i in range(1,len(hosts)):
-                log.info(f"Moving to {hosts[i]['ip']}")
-                child.sendline(f"telnet {hosts[i]['ip']}")
-                child.expect("Username:")
-                child.sendline(telnet_usr)
-                child.expect("Password:")
-                child.sendline(telnet_pwd)
-            child.sendline("config t")
-            child.expect(last_host["host"])
-            child.sendline(f"username {usr_to_update} privilege {privilege} password {new_pwd}")
-            child.expect(last_host["host"])
-            child.sendline("end")
-            child.sendline("wr mem")
-            child.expect(last_host['host'])
-            child.close()
-            log.info(f"Se ha terminado de modificar el usuario")
-            return make_response({"message": "Se ha modificado el usuario del router"}, 200)
-        except pexpect.TIMEOUT:
-            log.warning(f"Tiempo de espera excedido")
-            return make_response({"error": "Tiempo de espera en el router excedido"}, 500)
-        except Exception as ex:
-            log.error(str(ex))
-            return make_response({"error": "Ha ocurrido un error al ejecutar el comando"}, 500)
+
+        move_into_routers(child, hosts, gns3_usr, gns3_pwd, con_method)
+
+        child.sendline("config t")
+        child.expect(last_host["host"])
+        child.sendline(
+            f"username {usr_to_update} privilege {privilege} password {new_pwd}")
+        child.expect(last_host["host"])
+        child.sendline("end")
+        child.sendline("wr mem")
+        child.expect(last_host['host'])
+        child.close(force=True)
+        log.info(f"Se ha terminado de modificar el usuario via {con_method}")
+        return make_response({"message": "Se ha modificado el usuario en el router"}, 200)
+    except pexpect.TIMEOUT:
+        log.warning(f"Tiempo de espera excedido")
+        return make_response({"error": "Tiempo de espera en el router excedido"}, 500)
+    except Exception as ex:
+        log.error(str(ex))
+        return make_response({"error": "Ha ocurrido un error al ejecutar el comando"}, 500)
 
 @netapi_decorator("network")
 def delete_user_from_router(log=None):
@@ -151,50 +175,45 @@ def delete_user_from_router(log=None):
     request_body = request.get_json()
     hosts = request_body["hosts"]
     usr_to_delete = request_body["username"]
-    usr_pwd_to_delete = request_body["pwd"]
+    con_method = request_body["method"]
     last_host = hosts[len(hosts)-1]
     log.info(f"Eliminando usuario {usr_to_delete} del router con IP: {last_host['ip']}")
     # Verificar si se va a hacer por SSH y en el caso, obtener las credenciales
-    if ("ssh_usr", "ssh_pwd") in request_body:
-        #Hacerlo por SSH
-        pass
-    else:
-        #Hacerlo via telnet con la cuenta por defecto de todos los routers
-        telnet_usr, telnet_pwd = get_telnet_config()
-        try:
-            log.info(f"Enviando comandos al router: {hosts[0]['ip']}")
+    gns3_usr, gns3_pwd = get_gns3_config()
+    try:
+        if con_method == "ssh":
+            # Primer inicio de sesion via ssh al primer router
+            child = pxssh.pxssh()
+            child.login(hosts[0]["ip"], gns3_usr,
+                        gns3_pwd, auto_prompt_reset=False)
+        else:
+            # Inicio de sesion via telnet al primer router
             child = pexpect.spawn(f"telnet {hosts[0]['ip']}")
             child.expect("Username:")
-            child.sendline(telnet_usr)
+            child.sendline(gns3_usr)
             child.expect("Password:")
-            child.sendline(telnet_pwd)
+            child.sendline(gns3_pwd)
             child.expect(hosts[0]['host'])
-            # Siguientes hosts
-            for i in range(1,len(hosts)):
-                log.info(f"Moving to {hosts[i]['ip']}")
-                child.sendline(f"telnet {hosts[i]['ip']}")
-                child.expect("Username:")
-                child.sendline(telnet_usr)
-                child.expect("Password:")
-                child.sendline(telnet_pwd)
-            child.sendline("config t")
-            child.expect(last_host["host"])
-            child.sendline(f"no username {usr_to_delete} password {usr_pwd_to_delete}")
-            child.expect(last_host["host"])
-            child.sendline("end")
-            child.sendline("wr mem")
-            child.expect(last_host['host'])
-            child.close()
-            log.info(f"Se ha terminado de eliminar el usuario")
-            return make_response({"message": "Se ha eliminado el usuario del router"}, 200)
-        except pexpect.TIMEOUT:
-            log.warning(f"Tiempo de espera excedido")
-            return make_response({"error": "Tiempo de espera en el router excedido"}, 500)
-        except Exception as ex:
-            log.error(str(ex))
-            return make_response({"error": "Ha ocurrido un error al ejecutar el comando"}, 500)
 
+        move_into_routers(child, hosts, gns3_usr, gns3_pwd, con_method)
 
+        child.sendline("config t")
+        child.expect(last_host["host"])
+        child.sendline(f"no username {usr_to_delete}")
+        child.expect(last_host["host"])
+        child.sendline("end")
+        child.sendline("wr mem")
+        child.expect(last_host['host'])
+        child.close(force=True)
+        log.info(f"Se ha eliminado el usuario del router via {con_method}")
+        return make_response({"message": "Se ha eliminado el usuario en el router"}, 200)
+    except pexpect.TIMEOUT:
+        log.warning(f"Tiempo de espera excedido")
+        return make_response({"error": "Tiempo de espera en el router excedido"}, 500)
+    except Exception as ex:
+        log.error(str(ex))
+        return make_response({"error": "Ha ocurrido un error al ejecutar el comando"}, 500)
+    
 @netapi_decorator("network")
 def modify_router_protocol(log = None):
     session_data = validate_session()
@@ -205,82 +224,57 @@ def modify_router_protocol(log = None):
     hosts = request_body["hosts"]
     protocol = request_body["protocol"]
     networks = request_body["networks"]
+    con_method = request_body["method"]
     last_host = hosts[len(hosts)-1]
     log.info(f"Modificando protocolo en el router con IP: {last_host['ip']}")
-    # Verificar si se va a hacer por SSH y en el caso, obtener las credenciales
-    if ("ssh_usr", "ssh_pwd") in request_body:
-        #Hacerlo por SSH
-        pass
-    else:
-        #Hacerlo via telnet con la cuenta por defecto de todos los routers
-        telnet_usr, telnet_pwd = get_telnet_config()
-        try:
-            log.info(f"Enviando comandos al router: {hosts[0]['ip']}")
+    gns3_usr, gns3_pwd = get_gns3_config()
+    try:
+        if con_method == "ssh":
+            # Primer inicio de sesion via ssh al primer router
+            child = pxssh.pxssh()
+            child.login(hosts[0]["ip"], gns3_usr,
+                        gns3_pwd, auto_prompt_reset=False)
+        else:
+            # Inicio de sesion via telnet al primer router
             child = pexpect.spawn(f"telnet {hosts[0]['ip']}")
             child.expect("Username:")
-            child.sendline(telnet_usr)
+            child.sendline(gns3_usr)
             child.expect("Password:")
-            child.sendline(telnet_pwd)
+            child.sendline(gns3_pwd)
             child.expect(hosts[0]['host'])
-            # Siguientes hosts
-            for i in range(1,len(hosts)):
-                log.info(f"Moving to {hosts[i]['ip']}")
-                child.sendline(f"telnet {hosts[i]['ip']}")
-                child.expect("Username:")
-                child.sendline(telnet_usr)
-                child.expect("Password:")
-                child.sendline(telnet_pwd)
-            
-            # Router de destino
-            child.sendline("terminal length 0")
-            child.expect(last_host["host"])
-            log.info("Obteniendo protocolos del router")
-            child.sendline("show ip protocol")
-            child.expect(last_host["host"])
-            modified = False
-            protocol_info = child.before.decode()
-            child.sendline("conf t")
-            child.expect(last_host["host"])
-            if protocol_info != "":
-                log.info("Se ha encontrado valores de protocolos")
-                protocol_info = protocol_info.split("\n")
-                routing_protocols_data = [x for x in protocol_info if x.startswith("Routing Protocol")]
-                protocols_in_router = []
-                for prot in routing_protocols_data:
-                    temp_p = prot.split(" ")[3]
-                    temp_p = temp_p.replace("\r", "")
-                    temp_p = temp_p.replace("\"", "")
-                    protocols_in_router.append(temp_p)
-                
-                modified = len(protocols_in_router) > 0
-                log.info(f"Protocolos encontrado: {', '.join(protocols_in_router)}")
-                for pt_rt in protocols_in_router:
-                    log.info(f"Eliminando protocolo: {pt_rt}")
-                    child.sendline(f"no router {pt_rt}")
-                    child.expect(last_host["host"])
 
-            if protocol == "rip":
-                log.info("Anadiendo protocolo rip")
-                child.sendline("router rip")
-                child.sendline("ver 2")
-            if protocol.startswith("ospf") or protocol.startswith("eigrp"):
-                log.info(f"Anadiendo protocolo {protocol}")
-                child.sendline(f"router {protocol}")
-                child.expect(last_host["host"])
+        move_into_routers(child, hosts, gns3_usr, gns3_pwd, con_method)
 
-            for net in networks:
-                child.sendline(f"net {net}")
-                child.expect(last_host["host"])
-
-            child.sendline("end")
+        protocols_in_router = get_router_protocols(child, last_host["host"])
+        child.sendline("config t")
+        child.expect(last_host["host"])
+        has_to_modify = len(protocols_in_router) > 0
+        if has_to_modify:
+            delete_protocols_from_router(child, last_host["host"], protocols_in_router)
+        
+        if protocol == "rip":
+            log.info("Añadiendo protocolo rip")
+            child.sendline("router rip")
+            child.sendline("ver 2")
+        if protocol.startswith("ospf") or protocol.startswith("eigrp"):
+            log.info(f"Anadiendo protocolo {protocol}")
+            child.sendline(f"router {protocol}")
             child.expect(last_host["host"])
-            child.sendline("wr mem")
-            child.close()
-           
-            return make_response({"message": "Se ha modificado el protocolo existente" if modified else "Se ha anadido el protocolo"}, 200)
-        except pexpect.TIMEOUT:
-            log.warning(f"Tiempo de espera excedido")
-            return make_response({"error": "Tiempo de espera en el router excedido"}, 500)
-        except Exception as ex:
-            log.error(str(ex))
-            return make_response({"error": "Ha ocurrido un error al ejecutar el comando"}, 500)
+
+        for net in networks:
+            child.sendline(f"net {net}")
+            child.expect(last_host["host"])
+
+        child.sendline("end")
+        child.expect(last_host["host"])
+        child.sendline("wr mem")
+        child.close()
+        log.info(f"Se ha terminado de configurar el protocolo {protocol} con las redes {', '.join(networks)} via {con_method}")
+        return make_response({"message": "Se ha modificado el protocolo existente" if has_to_modify else "Se ha anadido el protocolo"}, 200)
+    except pexpect.TIMEOUT:
+        log.warning(f"Tiempo de espera excedido")
+        return make_response({"error": "Tiempo de espera en el router excedido"}, 500)
+    except Exception as ex:
+        log.error(str(ex))
+        return make_response({"error": "Ha ocurrido un error al ejecutar el comando"}, 500)
+    
