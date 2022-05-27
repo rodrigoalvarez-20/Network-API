d37c3573cf959727b69d14448db4e603af607bb5
ralvarez20 <rodrigoalvarez449@gmail.com>
2022-05-27 14:34:43 -0500

    Seccion de actualizacion de usuarios en el router (crear, modificar o eliminar)

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 from api.utils.configs import get_general_config
 from api.monitor import start_mapping
 from api.routes.configurations import get_all_app_config, update_configs
-from api.routes.routers import config_monitor, display_network, get_monitor_config, modify_router_config
+from api.routes.routers import config_monitor, display_network, get_monitor_config, modify_router_config, modify_users_in_router
 from api.routes.users import change_password, delete_user, get_users, login_user, register_user, send_reset_email, update_profile
 from api.utils.common import auth
 from api.utils.tokens_handler import search_used_token
@@ -42,16 +42,13 @@ app.add_url_rule("/api/app/configurations", "update_configs", update_configs, me
 # Seccion de routers
 app.add_url_rule("/api/routers/graph", "display_network", display_network, methods=["GET"])
 app.add_url_rule("/api/routers/config", "modify_router_config", modify_router_config, methods=["PATCH"])
+app.add_url_rule("/api/routers/users", "modify_users_in_router", modify_users_in_router, methods=["POST"])
 
 app.add_url_rule("/api/routers/monitor", "config_monitor", config_monitor, methods=["POST"])
 app.add_url_rule("/api/routers/monitor", "get_monitor_config", get_monitor_config, methods=["GET"])
 
-#app.add_url_rule("/api/routers/users/add", "add_user_to_router", add_user_to_router, methods=["POST"])
-#app.add_url_rule("/api/routers/users/modify", "update_user_from_router", update_user_from_router, methods=["POST"])
-#app.add_url_rule("/api/routers/users/delete", "delete_user_from_router", delete_user_from_router, methods=["DELETE"])
-#app.add_url_rule("/api/routers/protocol", "modify_router_protocol", modify_router_protocol, methods=["POST"])
-#app.add_url_rule("/api/routers/config/ssh", "activate_ssh_in_router", activate_ssh_in_router, methods=["POST"])
 
+#app.add_url_rule("/api/routers/protocol", "modify_router_protocol", modify_router_protocol, methods=["POST"])
 
 @app.get("/api")
 @netapi_decorator("general", None)
